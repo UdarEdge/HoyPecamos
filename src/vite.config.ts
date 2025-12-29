245099313365a55bc1bc7733c0d9d3e59a99f779
David <hola@udaredge.com>
2025-12-29 15:46:07 +0100

    Fix: Añadir root y emptyOutDir en vite.config

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
@@ -15,6 +16,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     sourcemap: false,
     rollupOptions: {
       output: {
